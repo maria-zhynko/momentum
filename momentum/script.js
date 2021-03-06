5530591a08baf9a54bbb51d1e6b17137021a39ee
maria-zhynko <66383351+maria-zhynko@users.noreply.github.com>
2021-03-06 12:28:28 +0300

    feat: refactor script

--- momentum/script.js
+++ momentum/script.js
@@ -1,4 +1,3 @@
-// DOM Elements
 const time = document.querySelector('.time'),
   greeting = document.querySelector('.greeting'),
   name = document.querySelector('.name'),
@@ -28,8 +27,10 @@ for(let i=0;i<6;i++){
   picsE.push(ind);
   picsN.push(ind);
 }
+let pics = picsN.concat(picsM,picsD,picsE);
+
   let today = new Date(),
-    hour_for_button = today.getHours();
+    hour_for_button = today.getHours(),
     hour_for_background = today.getHours();
 
 const showAmPm = true;
@@ -41,14 +42,6 @@ function showTime() {
     weekday=today.getDay(),
     day=today.getDate(),
     month=today.getMonth();
-
-    if(hour_for_background<hour){
-      setBgGreet();
-      if(hour_for_background!==23)
-      hour_for_background++;
-      else hour_for_background=0;
-    }
-    month++;
 
     let weekdaySTR=setDay(weekday);
     let monthSTR=setMonth(month);
@@ -81,8 +74,6 @@ else{
   weatherDescription.textContent = data.weather[0].description+`, humidity ${data.main.humidity}%, wind speed ${data.wind.speed}m/s`;
 }
  
-
-  
 }
 
 async function getQuote() {  
@@ -97,194 +88,60 @@ function addZero(n) {
 }
 
 function setDay(weekday){
-  let weekdaySTR; 
-  switch(weekday){
-    case 1:
-      weekdaySTR="Monday";
-      break;
-    case 2:
-      weekdaySTR="Tuesday";
-      break;
-    case 3:
-      weekdaySTR="Wednesday";
-      break;
-    case 4:
-      weekdaySTR="Thursday";
-      break;
-    case 5:
-      weekdaySTR="Friday";
-      break;
-    case 6:
-      weekdaySTR="Saturday";
-      break;
-    case 0:
-      weekdaySTR="Sunday";
-      break;
-  }
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  let weekdaySTR = days[weekday]; 
+  
   return weekdaySTR;
 }
 
 function setMonth(month){
-  let monthSTR;
-  switch(month){
-    case 1:
-      monthSTR="January";
-      break;
-    case 2:
-      monthSTR="February";
-      break;
-    case 3:
-      monthSTR="March";
-      break;
-    case 4:
-      monthSTR="April";
-      break;
-    case 5:
-      monthSTR="May";
-      break;
-    case 6:
-      monthSTR="June";
-      break;
-    case 7:
-      monthSTR="July";
-      break;
-    case 8:
-      monthSTR="August";
-      break;
-    case 9:
-      monthSTR="September";
-      break;
-    case 10:
-      monthSTR="October";
-      break;
-    case 11:
-      monthSTR="November";
-      break;
-    case 12:
-      monthSTR="December";
-      break;
-  }
+  let months = [
+    'January',
+    'Febrary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  let monthSTR = months[month];
+  
   return monthSTR;
 }
 
-function setBgGreet() {
-  let today = new Date(),
-    hour = today.getHours();
+function setBgGreet(hour) {
 
   if (hour >= 6 && hour < 12) {
-    switch (hour){
-      case 6:
-        document.body.style.backgroundImage =
-        `url('assets/images/morning/${picsM[0]}.jpg'),url(assets/images/overlay.png)`;
-        break;
-      case 7:
-        document.body.style.backgroundImage =
-        `url('assets/images/morning/${picsM[1]}.jpg')`;
-      break;
-      case 8:
-        document.body.style.backgroundImage =
-        `url('assets/images/morning/${picsM[2]}.jpg')`;
-      break;
-      case 9:
-        document.body.style.backgroundImage =
-        `url('assets/images/morning/${picsM[3]}.jpg'),url(assets/images/overlay.png)`;
-      break;
-      case 10:
-        document.body.style.backgroundImage =
-        `url('assets/images/morning/${picsM[4]}.jpg')`;
-      break;
-      case 11:
-        document.body.style.backgroundImage =
-        `url('assets/images/morning/${picsM[5]}.jpg')`;
-      break;
-    }
+    document.body.style.backgroundImage =
+    `url('assets/images/morning/${pics[hour]}.jpg')`;
     greeting.textContent = 'Good Morning, ';
   } else if (hour >= 12 && hour < 18) {
-    switch (hour){
-      case 12:
-        document.body.style.backgroundImage =
-        `url('assets/images/day/${picsD[0]}.jpg')`;
-      break;
-      case 13:
-        document.body.style.backgroundImage =
-        `url('assets/images/day/${picsD[1]}.jpg')`;
-      break;
-      case 14:
-        document.body.style.backgroundImage =
-        `url('assets/images/day/${picsD[2]}.jpg')`;
-      break;
-      case 15:
-        document.body.style.backgroundImage =
-        `url('assets/images/day/${picsD[3]}.jpg')`;
-      break;
-      case 16:
-        document.body.style.backgroundImage =
-        `url('assets/images/day/${picsD[4]}.jpg')`;
-      break;
-      case 17:
-        document.body.style.backgroundImage =
-        `url('assets/images/day/${picsD[5]}.jpg')`;
-      break;
-    }
+    document.body.style.backgroundImage =
+    `url('assets/images/day/${pics[hour]}.jpg')`;
     document.body.style.color = 'white';
     greeting.textContent = 'Good Afternoon, ';
   } else if (hour>=18&&hour<24){
-    switch (hour){
-      case 18:
-        document.body.style.backgroundImage =
-        `url('assets/images/evening/${picsE[0]}.jpg')`;
-      break;
-      case 19:
-        document.body.style.backgroundImage =
-        `url('assets/images/evening/${picsE[1]}.jpg')`;
-      break;
-      case 20:
-        document.body.style.backgroundImage =
-        `url('assets/images/evening/${picsE[2]}.jpg')`;
-      break;
-      case 21:
-        document.body.style.backgroundImage =
-        `url('assets/images/evening/${picsE[3]}.jpg')`;
-      break;
-      case 22:
-        document.body.style.backgroundImage =
-        `url('assets/images/evening/${picsE[4]}.jpg')`;
-      break;
-      case 23:
-        document.body.style.backgroundImage =
-        `url('assets/images/evening/${picsE[5]}.jpg')`;
-      break;
-    }
+    document.body.style.backgroundImage =
+    `url('assets/images/evening/${pics[hour]}.jpg')`;
     greeting.textContent = 'Good Evening, ';
     document.body.style.color = 'white';
   }
   else{
-    switch (hour){
-      case 0:
-        document.body.style.backgroundImage =
-        `url('assets/images/night/${picsN[0]}.jpg')`;
-      break;
-      case 1:
-        document.body.style.backgroundImage =
-        `url('assets/images/night/${picsN[1]}.jpg')`;
-      break;
-      case 2:
-        document.body.style.backgroundImage =
-        `url('assets/images/night/${picsN[2]}.jpg')`;
-      break;
-      case 3:
-        document.body.style.backgroundImage =
-        `url('assets/images/night/${picsN[3]}.jpg')`;
-      break;
-      case 4:
-        document.body.style.backgroundImage =
-        `url('assets/images/night/${picsN[4]}.jpg')`;
-      break;
-      case 5:
-        document.body.style.backgroundImage =
-        `url('assets/images/night/${picsN[5]}.jpg')`;
-      break;
-    }
+    document.body.style.backgroundImage =
+    `url('assets/images/night/${pics[hour]}.jpg')`;
     greeting.textContent = 'Good Night, ';
     document.body.style.color = 'white';
   }
@@ -390,105 +247,8 @@ function setFocus(e) {
 function change_background(){
   hour_for_button++;
   if(hour_for_button>=24)hour_for_button=0;
-
-  switch(hour_for_button){
-    case 0:
-      document.body.style.backgroundImage =
-      `url('assets/images/night/${picsN[0]}.jpg')`;
-    break;
-    case 1:
-      document.body.style.backgroundImage =
-      `url('assets/images/night/${picsN[1]}.jpg')`;
-    break;
-    case 2:
-      document.body.style.backgroundImage =
-      `url('assets/images/night/${picsN[2]}.jpg')`;
-    break;
-    case 3:
-      document.body.style.backgroundImage =
-      `url('assets/images/night/${picsN[3]}.jpg')`;
-    break;
-    case 4:
-      document.body.style.backgroundImage =
-      `url('assets/images/night/${picsN[4]}.jpg')`;
-    break;
-    case 5:
-      document.body.style.backgroundImage =
-      `url('assets/images/night/${picsN[5]}.jpg')`;
-    break;
-    case 6:
-      document.body.style.backgroundImage =
-      `url('assets/images/morning/${picsM[0]}.jpg')`;
-      break;
-    case 7:
-      document.body.style.backgroundImage =
-      `url('assets/images/morning/${picsM[1]}.jpg')`;
-    break;
-    case 8:
-      document.body.style.backgroundImage =
-      `url('assets/images/morning/${picsM[2]}.jpg')`;
-    break;
-    case 9:
-      document.body.style.backgroundImage =
-      `url('assets/images/morning/${picsM[3]}.jpg')`;
-    break;
-    case 10:
-      document.body.style.backgroundImage =
-      `url('assets/images/morning/${picsM[4]}.jpg')`;
-    break;
-    case 11:
-      document.body.style.backgroundImage =
-      `url('assets/images/morning/${picsM[5]}.jpg')`;
-    break;
-    case 12:
-      document.body.style.backgroundImage =
-      `url('assets/images/day/${picsD[0]}.jpg')`;
-    break;
-    case 13:
-      document.body.style.backgroundImage =
-      `url('assets/images/day/${picsD[1]}.jpg')`;
-    break;
-    case 14:
-      document.body.style.backgroundImage =
-      `url('assets/images/day/${picsD[2]}.jpg')`;
-    break;
-    case 15:
-      document.body.style.backgroundImage =
-      `url('assets/images/day/${picsD[3]}.jpg')`;
-    break;
-    case 16:
-      document.body.style.backgroundImage =
-      `url('assets/images/day/${picsD[4]}.jpg')`;
-    break;
-    case 17:
-      document.body.style.backgroundImage =
-      `url('assets/images/day/${picsD[5]}.jpg')`;
-    break;
-    case 18:
-      document.body.style.backgroundImage =
-      `url('assets/images/evening/${picsN[0]}.jpg')`;
-    break;
-    case 19:
-      document.body.style.backgroundImage =
-      `url('assets/images/evening/${picsN[1]}.jpg')`;
-    break;
-    case 20:
-      document.body.style.backgroundImage =
-      `url('assets/images/evening/${picsN[2]}.jpg')`;
-    break;
-    case 21:
-      document.body.style.backgroundImage =
-      `url('assets/images/evening/${picsN[3]}.jpg')`;
-    break;
-    case 22:
-      document.body.style.backgroundImage =
-      `url('assets/images/evening/${picsN[4]}.jpg')`;
-    break;
-    case 23:
-      document.body.style.backgroundImage =
-      `url('assets/images/evening/${picsN[5]}.jpg')`;
-    break;
-  }  
+  setBgGreet(hour_for_button);
+  
 }
 
 name.addEventListener('click', clearName);
@@ -502,7 +262,7 @@ town.addEventListener('keypress', setCity);
 town.addEventListener('blur', getCity);
 
 showTime();
-setBgGreet();
+setBgGreet(hour_for_background);
 getName();
 getFocus();
 getCity();
